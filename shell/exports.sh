e0ed5382a5bab53fb74caa850d561a22a54cce91
Ricardo Ruwer <ricardoruwer@gmail.com>
2019-09-03 11:31:17 -0300

    Add go path

--- shell/exports.sh
+++ shell/exports.sh
@@ -10,11 +10,14 @@ export HISTSIZE=10000 # Store 10k history entries
 
 export KERL_CONFIGURE_OPTIONS="--disable-debug --without-javac"
 
+export GOPATH=~/go
+
 # PATH
 export PATH="$HOME/.bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="./bin:$PATH"
 export PATH="/usr/local/opt/postgresql@9.6/bin:$PATH"
+export PATH=$PATH:$GOPATH/bin
 
 if [ $ITERM_SESSION_ID ]; then
   export PROMPT_COMMAND='echo -ne "\033];${PWD##*/}\007"; ':"$PROMPT_COMMAND";
