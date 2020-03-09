70877a40d8a93092a5f09f4aa633488d06d7d8c8
Ricardo Ruwer <ricardoruwer@gmail.com>
2020-03-09 12:37:29 -0300

    Put exports in the right place

--- shell/exports.sh
+++ shell/exports.sh
@@ -8,8 +8,8 @@ export HISTFILE="$HOME/.zsh_history"
 export HISTCONTROL="ignoreboth:erasedups" # Erase duplicates in history
 export HISTSIZE=10000 # Store 10k history entries
 
+export ERL_AFLAGS="-kernel shell_history enabled"
 export KERL_CONFIGURE_OPTIONS="--disable-debug --without-javac"
-
 export GOPATH=~/go
 
 export DIRENV_LOG_FORMAT=
@@ -19,6 +19,7 @@ export PATH="$HOME/.bin:$PATH"
 export PATH="$HOME/.bin-private:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="./bin:$PATH"
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH=$PATH:$GOPATH/bin
 
 if [ $ITERM_SESSION_ID ]; then
