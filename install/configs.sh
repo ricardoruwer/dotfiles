8ad12430555a77d028c92eba13a90576291da2f4
Ricardo Ruwer <ricardoruwer@gmail.com>
2022-04-13 13:56:21 -0300

    Import GPG key

--- install/configs.sh
+++ install/configs.sh
@@ -4,16 +4,23 @@ source ~/.dotfiles/shell/functions.sh
 for file in ~/.dotfiles/configs/*; do
   if [ -r "$file" ] && [ -f "$file" ]; then
     target="${HOME}/.${file##*/}"
-
     echo "${Green}Linking ${target}${ColorOff}"
     ln -sf $file $target
   fi
 done
 
-# Itermocil is not on /configs because it has some private things
-echo "${Blue}=> Configuring itermocil${ColorOff}"
+# Log in to Google Drive to import private things from there
+echo "${Blue}=> Setting up Google Drive${ColorOff}"
 while [ ! -d /Volumes/GoogleDrive/My\ Drive ]; do
   confirm "${Yellow}Please open and log in to Google Drive before continuing${ColorOff}"
 done
+
+# Set up Itermocil
 echo "${Green}Linking ${HOME}/.itermocil${ColorOff}"
 ln -sf /Volumes/GoogleDrive/My\ Drive/.itermocil ~/.itermocil
+
+# Import GPG key
+echo "${Green}Importing GPG key${ColorOff}"
+pushd /Volumes/GoogleDrive/My\ Drive/Dev
+gpg --import gpg.key
+popd
