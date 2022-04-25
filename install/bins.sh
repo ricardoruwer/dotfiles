e5204043335ad851ce1edee4c2fd6f5f6255f614
Ricardo Ruwer <ricardoruwer@gmail.com>
2022-04-25 13:29:20 -0300

    Fix GoogleDrive dir

--- install/bins.sh
+++ install/bins.sh
@@ -4,8 +4,8 @@ echo "${Green}Linking ~/.bin${ColorOff}"
 ln -sfn ~/.dotfiles/bin ~/.bin
 
 echo "${Green}Linking ~/.bin-private${ColorOff}"
-while [ ! -d /Volumes/GoogleDrive/My\ Drive ]; do
+while [ ! -d ~/Google\ Drive/My\ Drive ]; do
   confirm "${Yellow}Please open and log in to Google Drive before continuing${ColorOff}"
 done
-ln -sfn /Volumes/GoogleDrive/My\ Drive/bin ~/.dotfiles/bin-private
+ln -sfn ~/Google\ Drive/My\ Drive/bin ~/.dotfiles/bin-private
 ln -sfn ~/.dotfiles/bin-private ~/.bin-private
