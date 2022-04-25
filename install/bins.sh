e7c38e3574824744169ba2f5ae1330512bfd1ec6
Ricardo Ruwer <ricardoruwer@gmail.com>
2022-04-25 16:46:04 -0300

    Remove private bins from GoogleDrive

--- install/bins.sh
+++ install/bins.sh
@@ -4,8 +4,6 @@ echo "${Green}Linking ~/.bin${ColorOff}"
 ln -sfn ~/.dotfiles/bin ~/.bin
 
 echo "${Green}Linking ~/.bin-private${ColorOff}"
-while [ ! -d ~/Google\ Drive/My\ Drive ]; do
-  confirm "${Yellow}Please open and log in to Google Drive before continuing${ColorOff}"
-done
-ln -sfn ~/Google\ Drive/My\ Drive/bin ~/.dotfiles/bin-private
+mkdir -p ~/.dotfiles/bin-private
+confirm "${Yellow}Add your private bins into the ${Blue}~/.dotfiles/bin-private ${Yellow}folder${ColorOff}"
 ln -sfn ~/.dotfiles/bin-private ~/.bin-private
