f027197f78f8a061460618057ff423cf3d500bce
Ricardo Ruwer <ricardoruwer@gmail.com>
2024-02-14 13:00:41 -0300

    Add title to apps

--- install/apps.sh
+++ install/apps.sh
@@ -54,12 +54,14 @@ case "$1" in
     ;;
 
   zsh-plugins)
+    echo "${Blue}=> Installing ZSH plugins${ColorOff}"
     pushd ~/.oh-my-zsh/custom/plugins
     git clone https://github.com/bernardop/iterm-tab-color-oh-my-zsh.git iterm-tab-color
     popd
     ;;
 
   spaceship)
+    echo "${Blue}=> Installing spaceship theme${ColorOff}"
     git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
     ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
     ;;
