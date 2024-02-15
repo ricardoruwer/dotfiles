cd0cf3038263dff8ba4aff96986c25933b084e1e
Ricardo Ruwer <ricardoruwer@gmail.com>
2024-02-15 14:38:35 -0300

    Add some more exports

--- shell/exports.sh
+++ shell/exports.sh
@@ -3,21 +3,20 @@ export EDITOR="code"
 export BUNDLER_EDITOR="$EDITOR"
 export GEM_EDITOR="$EDITOR"
 export VISUAL="$EDITOR"
-
 export HISTFILE="$HOME/.zsh_history"
 export HISTCONTROL="ignoreboth:erasedups" # Erase duplicates in history
 export HISTSIZE=10000 # Store 10k history entries
-
 export GPG_TTY=$(tty)
 
 # ASDF
 export KERL_CONFIGURE_OPTIONS="--without-javac --with-ssl=$(brew --prefix openssl@1.1)"
+export ASDF_GOLANG_MOD_VERSION_ENABLED=true
 
 # WORK
 export RUBY_CONFIGURE_OPTS="--with-jemalloc"
 export GOPATH="$HOME/go"
 export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
-
+export RUBYOPT='-W0'
 export TRR_DIR="$HOME/Code/trr"
 
 # DIRENV
